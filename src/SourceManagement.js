2318b479ead3115ded22c2e0209195047f017822
Faktor 10 <oreillyross@gmail.com>
2020-01-16 11:42:23 +0100

    add React Table

--- src/SourceManagement.js
+++ src/SourceManagement.js
@@ -1,26 +1,92 @@
 import React from "react";
-import { Grid, Button, Divider } from "semantic-ui-react";
-import {
-  useTable,
-  useGroupBy,
-  useFilters,
-  useSortBy,
-  useExpanded,
-  usePagination
-} from "react-table";
-import Sources from "./Sources";
+import { useTable } from "react-table";
+import makeData from "./makedata";
 
-export default () => (
-  <div>
-    <h4>Source Management</h4>
-    <Grid columns={1} divided>
-      <Grid.Row>
-        <Grid.Column>
-          Sources
-          <Divider />
-          <Sources />
-        </Grid.Column>
-      </Grid.Row>
-    </Grid>
-  </div>
-);
+function Table({ columns, data }) {
+  // Use the state and functions returned from useTable to build your UI
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow
+  } = useTable({
+    columns,
+    data
+  });
+
+  // Render the UI for your table
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map(headerGroup => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map(column => (
+              <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row, i) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map(cell => {
+                return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
+
+export default () => {
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: "Name",
+        columns: [
+          {
+            Header: "First Name",
+            accessor: "firstName"
+          },
+          {
+            Header: "Last Name",
+            accessor: "lastName"
+          }
+        ]
+      },
+      {
+        Header: "Info",
+        columns: [
+          {
+            Header: "Age",
+            accessor: "age"
+          },
+          {
+            Header: "Visits",
+            accessor: "visits"
+          },
+          {
+            Header: "Status",
+            accessor: "status"
+          },
+          {
+            Header: "Profile Progress",
+            accessor: "progress"
+          }
+        ]
+      }
+    ],
+    []
+  );
+  return (
+    <div>
+      <h4>Source Management</h4>
+      <Table columns={columns} data={makeData(10)} />
+    </div>
+  );
+};
