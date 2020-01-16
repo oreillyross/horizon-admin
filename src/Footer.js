82ee71f7c5b55d6b2c1eeb9693ef92e0858d99ca
Faktor 10 <oreillyross@gmail.com>
2020-01-16 15:08:01 +0100

    formik added for source form

--- src/Footer.js
+++ src/Footer.js
@@ -1,17 +1,34 @@
-import React from "react";
-import { Button, Icon } from "semantic-ui-react";
+import React, { useState } from "react";
+import { Button, Icon, Modal } from "semantic-ui-react";
 import styled from "styled-components";
+import SourceForm from "./forms/SourceForm";
 
 const StyledFooter = styled.div`
   color: red;
 `;
 
 export default function Footer() {
+  const [modalOpen, setOpen] = useState(false);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
   return (
     <StyledFooter>
-      <Button icon>
-        <Icon name="add" /> New Source
-      </Button>
+      <Modal
+        open={modalOpen}
+        trigger={
+          <Button icon onClick={() => setOpen(true)}>
+            <Icon name="add" /> New Source
+          </Button>
+        }
+      >
+        <Modal.Header>Add a New Source</Modal.Header>
+        <Modal.Content image>
+          <SourceForm handleClose={handleClose} />
+        </Modal.Content>
+      </Modal>
+      <SourceForm />
     </StyledFooter>
   );
 }
--- src/forms/SourceForm.js
+++ src/forms/SourceForm.js
@@ -1,10 +1,40 @@
 import React from "react";
-import { Formik } from "formik";
+import { useFormik } from "formik";
+import { Form, Label, Button } from "semantic-ui-react";
 
-export default function SourceForm() {
+export default function SourceForm({ handleClose }) {
+  // Pass the useFormik() hook initial form values and a submit function that will
+  // be called when the form is submitted
+  const formik = useFormik({
+    initialValues: {
+      sourceName: "",
+      sourceUrl: ""
+    },
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2));
+    }
+  });
+
   return (
-    <Formik>
-      <div>Source Form</div>
-    </Formik>
+    <Form onSubmit={formik.handleSubmit}>
+      <Label htmlFor="sourceName">Source Name</Label>
+      <input
+        id="sourceName"
+        name="sourceName"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.sourceName}
+      />
+      <Label htmlFor="sourceUrl">Source Url</Label>
+      <input
+        id="sourceUrl"
+        name="sourceUrl"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.sourceUrl}
+      />
+      <Button type="submit">Submit</Button>
+      <Button onClick={handleClose}>Cancel</Button>
+    </Form>
   );
 }
