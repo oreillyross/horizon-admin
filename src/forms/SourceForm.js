f1189f029643a2a46f8ae1da824648b0c93fb8ce
Faktor 10 <oreillyross@gmail.com>
2020-01-17 15:35:39 +0100

    add field level validation

--- src/forms/SourceForm.js
+++ src/forms/SourceForm.js
@@ -2,6 +2,19 @@ import React from "react";
 import { useFormik } from "formik";
 import { Form, Label, Button } from "semantic-ui-react";
 
+const validate = values => {
+  const errors = {};
+  if (values.sourceName === "") {
+    errors.sourceName = "Source Name cannot be empty";
+  }
+  const expression = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+  const regex = new RegExp(expression);
+  if (!values.sourceUrl.match(regex)) {
+    errors.sourceUrl = "The url is not valid";
+  }
+  return errors;
+};
+
 export default function SourceForm({ handleClose }) {
   // Pass the useFormik() hook initial form values and a submit function that will
   // be called when the form is submitted
@@ -12,29 +25,44 @@ export default function SourceForm({ handleClose }) {
     },
     onSubmit: values => {
       alert(JSON.stringify(values, null, 2));
-    }
+    },
+    validate
   });
 
   return (
     <Form onSubmit={formik.handleSubmit}>
-      <Label htmlFor="sourceName">Source Name</Label>
-      <input
-        id="sourceName"
-        name="sourceName"
-        type="text"
-        onChange={formik.handleChange}
-        value={formik.values.sourceName}
-      />
-      <Label htmlFor="sourceUrl">Source Url</Label>
-      <input
-        id="sourceUrl"
-        name="sourceUrl"
-        type="text"
-        onChange={formik.handleChange}
-        value={formik.values.sourceUrl}
-      />
-      <Button type="submit">Submit</Button>
-      <Button onClick={handleClose}>Cancel</Button>
+      <Form.Field>
+        <label htmlFor="sourceName">Source Name</label>
+        <input
+          id="sourceName"
+          name="sourceName"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.sourceName}
+          placeholder="Source Name"
+        />
+        {formik.errors.sourceName ? (
+          <div>{formik.errors.sourceName}</div>
+        ) : null}
+      </Form.Field>
+      <Form.Field>
+        <label htmlFor="sourceUrl">Source Url</label>
+        <input
+          id="sourceUrl"
+          name="sourceUrl"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.sourceUrl}
+          placeholder="Source Url"
+        />
+        {formik.errors.sourceUrl ? <div>{formik.errors.sourceUrl}</div> : null}
+      </Form.Field>
+      <Button color="linkedin" type="submit">
+        Submit
+      </Button>
+      <Button color="grey" onClick={handleClose}>
+        Cancel
+      </Button>
     </Form>
   );
 }
