59dfd757dd5505cdd505fa81f03761c3524b4131
Faktor 10 <oreillyross@gmail.com>
2020-08-24 13:36:57 +0200

    add

---
+++ src/model-data.js
@@ -0,0 +1,11 @@
+export const article = {
+  id: "c72da0f4-df38-4414-bad4-3019293c628f",
+  published: "2020-02-13",
+  title: "Daryan Meets Diab, Says He's 'PM of Entire Lebanon'",
+  text: "Article text blah blah blah...",
+  author: "some Author",
+  url: "http:/blahBlah.com",
+  crawled: "2020-02-13T14:24:00+00:00",
+  source_id: "42bc494e-4515-492f-b12e-f2decbd115ad",
+  status: "New" 
+}
